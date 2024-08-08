79f30589258e7771e9ac19e3be36eb2d943038b8
JunDemi <jungwook3176@gmail.com>
2024-08-08 14:48:02 +0900

    computed학습 (데이터 캐싱)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
-      <li v-for="data1 in testArray1" :key="data1">{{ data1 }}</li>
-      <hr/>
-      <li v-for="(data2, index) in testArray2" :key="data2">
-        {{ data2.id + " / " +data2.name }}
-      </li>
+     <h1>{{ text }}</h1>
+     <h1>{{ textChange() }}</h1>
+     <h1>{{ textChange() }}</h1>
+     <h1>{{ textChange() }}</h1>
+     <hr/>
+     <h1>{{ computedText }}</h1>
+     <h1>{{ computedText }}</h1>
+     <h1>{{ computedText }}</h1>
   </div>
 </template>
 <script>
 export default {
   data(){
     return {
-      testArray1: ['김예진', '이새봄', '박정욱', '권경민', '국다빈', '김정우', '최원준', '편선아'],
-      testArray2: [
-        { id: 1, name: "김예진"},
-        { id: 2, name: "이새봄"},
-        { id: 3, name: "박정욱"},
-        { id: 4, name: "권경민"},
-        { id: 5, name: "국다빈"},
-        { id: 6, name: "김정우"},
-        { id: 7, name: "최원준"},
-        { id: 8, name: "편선아"},
-      ],
+      text: "이니셜 키워드"
     }
+  },
+  //method와 달리 캐싱 기능이 있음
+  computed: {
+    computedText() {
+      console.log("computed 호출");
+      return this.text.split('').reverse().join('');
+    }
+  },
+  methods: {
+    textChange() {
+      console.log("methods 호출");
+      return this.text.split('').reverse().join('');
+    },
   }
 }
 </script>
