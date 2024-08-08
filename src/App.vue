e304a03ecc0410db38570485ef899a2507204be8
JunDemi <jungwook3176@gmail.com>
2024-08-08 15:11:41 +0900

    부모컴포넌트 -> 자식 컴포넌트 props전달 학습

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div>
-    <h1 @click="changeMessage">{{ message }}</h1>
-    <h2>{{ setMessage }}</h2>
-  </div>
+  <h1>부모</h1>
+  <hr/>
+  <Child v-bind:sendProps1="title" v-bind:sendProps2="createAt" :sendProps3="user"/>
 </template>
+
 <script>
+import Child from "./components/child.vue";
+
 export default {
-  data() {
+  components: {
+    Child,
+  },
+  data(){
     return {
-      message: "이니셜 문구",
-      setMessage: '',
-    }
-  },
-  //동적인 변경 데이터 감지
-  //computed로 변경된 데이터도 감지
-  watch: {
-    message(){
-      this.setMessage = "watch 호출"
-    }
-  },
-  methods: {
-    changeMessage() {
-      console.log('methods 호출');
-      this.message = "변경된 문구";
+      title: "부모 컴포넌트 텍스트",
+      createAt: "2024-08",
+      user: {
+        id: "3176thomas",
+        name: "정욱"
+      }
     }
   }
 }
---
+++ src/components/child.vue
@@ -0,0 +1,28 @@
+<template>
+    <h2>자식</h2>
+    <div>{{ sendProps1 }}</div>
+    <div>{{ sendProps2 }}</div>
+    <hr/>
+    <div>{{ sendProps3.id }}</div>
+    <div>{{ sendProps3.name }}</div>
+</template>
+<script>
+export default {
+    props: {
+        sendProps1: String,
+        sendProps2: String,
+        sendProps3: {
+            id: String,
+            name: String,
+        },
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
