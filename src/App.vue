8a4ce030236a4ce0702e8321ee1630b801ce51c0
JunDemi <jungwook3176@gmail.com>
2024-08-08 16:45:11 +0900

    faet: vue-router설치, 라우터 동작

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <div>
-    <input type="text" v-model="inputValue1"/>
-    <input type="text" :value="inputValue2" @input="inputValue2 = $event.target.value"/>
-    <div>{{ inputValue1 }}</div>
-    <div>{{ inputValue2 }}</div>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <hr />
+  <router-view />
 </template>
 <script>
 export default {
-  data() {
-    return {
-      inputValue1: "",
-      inputValue2: ""
-    }
-  }
+
 }
 </script>
 <style lang="">
-  
+
 </style>
