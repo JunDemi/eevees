8a6195de94fcdaf1d706e415d627627701e2abf0
JunDemi <jungwook3176@gmail.com>
2024-08-08 14:29:06 +0900

    반복 렌더링(v-for)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,31 @@
 <template>
-  <section>
-    <div v-show="isVisible" class="case1"></div>
-    <div v-show="!isVisible" class="case2"></div>
-    <button @click="setChange">변경</button>
-  </section>
+  <div>
+      <li v-for="data1 in testArray1" :key="data1">{{ data1 }}</li>
+      <hr/>
+      <li v-for="(data2, index) in testArray2" :key="data2">
+        {{ data2.id + " / " +data2.name }}
+      </li>
+  </div>
 </template>
 <script>
 export default {
-  data() {
+  data(){
     return {
-      isVisible: false,
+      testArray1: ['김예진', '이새봄', '박정욱', '권경민', '국다빈', '김정우', '최원준', '편선아'],
+      testArray2: [
+        { id: 1, name: "김예진"},
+        { id: 2, name: "이새봄"},
+        { id: 3, name: "박정욱"},
+        { id: 4, name: "권경민"},
+        { id: 5, name: "국다빈"},
+        { id: 6, name: "김정우"},
+        { id: 7, name: "최원준"},
+        { id: 8, name: "편선아"},
+      ],
     }
-  },
-  methods: {
-    setChange() {
-      this.isVisible = !this.isVisible;
-    },
   }
 }
 </script>
-<style scoped>
- section div{
-  width: 300px;
-  height: 100px;
- }
- .case1{
-  background-color: pink;
- }
- .case2{
-  background-color: lightgreen;
- }
- .case3{
-  background-color: skyblue;
- }
+<style>
+  
 </style>
