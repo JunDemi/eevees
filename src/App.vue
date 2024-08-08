c89202756d13ed10c53bbf13ebb442a4968b539a
JunDemi <jungwook3176@gmail.com>
2024-08-08 14:09:19 +0900

    조건부 렌더링 (v-if, v-else)

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,41 @@
 <template>
-  <div>
-    {{ case1 }}
-  </div>
-  <h1 v-html="case2"></h1>
-  <hr/>
-  <h2 :class="{active: isActive}">클래스 바인딩 테스트</h2>
-  <button @click="getChange">클릭</button>
-  <hr/>
-  <h2 :style="{color: fontColor, fontSize: fontSize + 'px'}">스타일 바인딩 테스트</h2>
+  <section>
+    <!-- <div class="case1" v-if="isVisible">true값</div>
+    <div class="case2" v-if="isVisible">true값</div>
+    <div class="case3" v-else>false값</div> -->
+
+    <div v-if="count === 2" class="case1"></div>
+    <div v-else class="case2"></div>
+    <button @click="count++">변경</button>
+  </section>
 </template>
 <script>
 export default {
   data() {
     return {
-      case1: "테스트 텍스트 1", //엘리먼트에 이중 중괄호는 텍스트 형태로 해석하여 출력
-      case2: "<p style='color: red'>테스트 텍스트 2</p>", //v-html은 해당 반환값에 대한 선언적 렌더링
-      isActive: false,
-      fontColor: '#888888',
-      fontSize: 48
+      isVisible: false,
+      count: 0
     }
   },
   methods: {
-    getChange() {
-      this.isActive = !this.isActive
-    }
+    setChange() {
+      this.isVisible = !this.isVisible;
+    },
   }
 }
 </script>
 <style scoped>
-  h2.active{
-    color: green;
-  }
+ section div{
+  width: 300px;
+  height: 100px;
+ }
+ .case1{
+  background-color: pink;
+ }
+ .case2{
+  background-color: lightgreen;
+ }
+ .case3{
+  background-color: skyblue;
+ }
 </style>
