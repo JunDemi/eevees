1bd634c94fbeb4c49ea7845ce7fdacb3c35bc23f
JunDemi <jungwook3176@gmail.com>
2024-08-08 13:26:13 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+  <h1>
+    Vue
+  </h1>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  beforeCreate() {
+      console.log("컴포넌트 생성 이전" , this.count);
+      this.test();
+    },
+  created(){
+    console.log("컴포넌트 생성 후" , this.count);
+  },
+  // beforeMount() {
+  //   console.log("HTML엘리먼트 마운트 전" , document.querySelector('h1'));
+  // },
+  // mounted() {
+  //   console.log("HTML엘리먼트 마운트 후" , document.querySelector('h1'));
+  // },
+}
+</script>
+<style lang="scss" scoped></style>
