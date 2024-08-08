6a2de73c7561c7664b0b2fdb852ed54edc706dd3
JunDemi <jungwook3176@gmail.com>
2024-08-08 15:21:04 +0900

    props전달 방법 optionsAPI -> compositionAPI문법으로 변경

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,35 @@
   <Child v-bind:sendProps1="title" v-bind:sendProps2="createAt" :sendProps3="user"/>
 </template>
 
-<script>
+<script setup>
+import { reactive, ref } from "vue";
 import Child from "./components/child.vue";
 
-export default {
-  components: {
-    Child,
-  },
-  data(){
-    return {
-      title: "부모 컴포넌트 텍스트",
-      createAt: "2024-08",
-      user: {
-        id: "3176thomas",
-        name: "정욱"
-      }
-    }
-  }
-}
+//composition API
+const title = ref("부모 컴포넌트 텍스트");
+const createAt = ref("2024-08");
+//반응성을 가진 객체는 ref보단 reactive사용 권장
+const user = reactive({ 
+  id: "3176thomas",
+  name: "정욱"
+});
+
+// // optionalAPI
+// export default {
+//   components: {
+//     Child,
+//   },
+//   data(){
+//     return {
+//       title: "부모 컴포넌트 텍스트",
+//       createAt: "2024-08",
+//       user: {
+//         id: "3176thomas",
+//         name: "정욱"
+//       }
+//     }
+//   }
+// }
 </script>
 <style>
   
