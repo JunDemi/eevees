1ca0738ac5ee1be97f2f2707786f8d782dea3e6a
JunDemi <jungwook3176@gmail.com>
2024-08-08 13:50:04 +0900

    클래스 바인딩 테스트

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,28 @@
   <div>
     {{ case1 }}
   </div>
-  <h1 v-html="case2">
-  </h1>
+  <h1 v-html="case2"></h1>
+  <h2 :class="{active: isActive}">클래스 바인딩 테스트</h2>
+  <button @click="getChange">클릭</button>
 </template>
 <script>
 export default {
   data() {
     return {
       case1: "테스트 텍스트 1", //엘리먼트에 이중 중괄호는 텍스트 형태로 해석하여 출력
-      case2: "<p style='color: red'>테스트 텍스트 2</p>" //v-html은 해당 반환값에 대한 선언적 렌더링
+      case2: "<p style='color: red'>테스트 텍스트 2</p>", //v-html은 해당 반환값에 대한 선언적 렌더링
+      isActive: false,
     }
   },
   methods: {
-    test() {
-      console.log("테스트 함수입니당");
-    },
+    getChange() {
+      this.isActive = !this.isActive
+    }
   }
 }
 </script>
-<style lang="scss" scoped></style>
+<style scoped>
+  h2.active{
+    color: green;
+  }
+</style>
