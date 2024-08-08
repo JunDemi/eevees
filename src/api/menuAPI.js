eacfbc20ab9005a97edf43f42e88f2edae3aa989
JunDemi <jungwook3176@gmail.com>
2024-08-09 00:37:30 +0900

    style: 폰트 적용 / feat: 메뉴 배열 api폴더에 저장 후 임포트

---
+++ src/api/menuAPI.js
@@ -0,0 +1,65 @@
+export const menuAPI = [
+    {
+        name: "이브이",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(170, 82, 0, 0.7), rgba(170, 82, 0, 0.7)), url(/public/menuImgs/이브이.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/O4Jd_KajtxifyyLFO1C3ojCGtHl-pKscr3324lDb_eUZBe0c-_ED8YwDBqmYG9pT9jBe916D4lIZ637hHomEBw.webp"
+    },
+    {
+        name: "샤미드",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(0, 84, 161, 0.7), rgba(0, 84, 161, 0.7)), url(/public/menuImgs/샤미드.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/Z2ZtH0QG_GWWB-BKEw-lgk0Lewb-nv8jctwteTEVAtXYbPWbjlTrbLPnUB5no5J5Vp4nznnHPQ5jH_vgwn-S8A.webp"
+    },
+    {
+        name: "쥬피썬더",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(142, 144, 0, 0.7), rgba(142, 144, 0, 0.7)), url(/public/menuImgs/쥬피썬더.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/_k5VLztKcq5w8cuuCIDs90FP3iUU1OWvE2Xi2fGGxC5d3DEdMAQ4_fzC9yqNvQOntEJEZM8gk_j9ZaclfgKf3Q.webp"
+    },
+    {
+        name: "부스터",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(166, 60, 0, 0.7), rgba(166, 60, 0, 0.7)), url(/public/menuImgs/부스터.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/oF6E-p9wvx_0L4GkjUpLDPZimxTWfTK6omDzvRbFuvDdlzhP3v2CjdFPPftSW4zxlimuDcZrM_abVQ-idTuwvA.webp"
+    },
+    {
+        name: "에브이",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(173, 0, 168, 0.7), rgba(173, 0, 168, 0.7)), url(/public/menuImgs/에브이.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/JtYBqIl3Ek5mtlDbkgE_HqbqIOFPeWZ0_kJoII3uo4aEpyA--DQWY482PAIL7WNpGLb2_kjRKoS4T7jZyoZ-oQ.webp"
+    },
+    {
+        name: "블래키",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(102, 102, 102, 0.7), rgba(102, 102, 102, 0.7)), url(/public/menuImgs/블래키.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/oh4aXHAW47ZIKvqWUp1-qEJecFdCvdiwzsHXWM0S1cbQTs9CYs8Snon-HfqSxFgLDKx4ojIkHQBvHLYNt5pMAQ.webp"
+    },
+    {
+        name: "리피아",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(38, 144, 0, 0.7), rgba(38, 144, 0, 0.7)), url(/public/menuImgs/리피아.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/4Bb7NkJX5TM9Knmgpa5JbvqGYiYDHfwlsrmflf6Xo7qG5u-hzaZ79wO-tuTdpUUatvnZyFDrHqnL8l6zBCI65w.webp"
+    },
+    {
+        name: "글레이시아",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(39, 172, 164, 0.7), rgba(39, 172, 164, 0.7)), url(/public/menuImgs/글레이시아.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/6cSYp5KHATKoYhNKFwrJKgbH6jwkePz9kmJ1Cuwn8gxqxqib5YK56WjZjbA77NH8CXspT3t_mKoaFdVgCoeouQ.webp"
+    },
+    {
+        name: "님피아",
+        menuStyle: {
+            backgroundImage: "linear-gradient(rgba(169, 98, 128, 0.7), rgba(169, 98, 128, 0.7)), url(/public/menuImgs/님피아.webp)"
+        },
+        modelImg: "https://i.namu.wiki/i/nvZCS-dV6_XQnBtwIQCKQlWoAmeZRaIftT2WiZ5rX29qDJcFl9XRlYE0e24_j4LSkV9RK3IVmTMAfLmjVmxcfg.webp"
+    }
+]
