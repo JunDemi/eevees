96f340dbd8d87359398187af71e2a5231a7fd67e
JunDemi <jungwook3176@gmail.com>
2024-08-08 15:27:29 +0900

    props전달 방법 optionsAPI -> compositionAPI문법으로 변경

--- src/components/child.vue
+++ src/components/child.vue
@@ -1,12 +1,14 @@
 <template>
     <h2>자식</h2>
-    <div>{{ props.sendProps1 }}</div>
-    <div>{{ props.sendProps2 }}</div>
+    <div>{{ sendProps1 }}</div>
+    <div>{{ sendProps2 }}</div>
     <hr/>
-    <div>{{ props.sendProps3.id }}</div>
-    <div>{{ props.sendProps3.name }}</div>
+    <div>{{ sendProps3.id }}</div>
+    <div>{{ sendProps3.name }}</div>
 </template>
 <script setup lang="ts">
+import { toRefs } from 'vue';
+
 interface Props{
     sendProps1: String;
     sendProps2: String;
@@ -16,6 +18,10 @@ interface Props{
     }
 }
 const props = defineProps<Props>();
+
+//toRefs: 전달받은 props의 객체 구조 분해
+const {sendProps1, sendProps2, sendProps3} = toRefs(props);
+
 </script>
 <style lang="">
     
