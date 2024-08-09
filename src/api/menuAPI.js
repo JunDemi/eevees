8efffc0b8db07509b4e3f022dab38e74ca21df36
JunDemi <jungwook3176@gmail.com>
2024-08-09 22:53:13 +0900

    chore: 도감 이미지 그라데이션 삭제, 부스터/쥬피썬더 도감 이미지 변경

--- src/api/menuAPI.js
+++ src/api/menuAPI.js
@@ -8,7 +8,7 @@ export const menuAPI = [
     },
     dexBacground: {
       backgroundImage:
-        "linear-gradient(rgba(170, 82, 0, 0.7), rgba(170, 82, 0, 0.7)), url('src/static/menuImgs/dexBg/이브이.webp')",
+        "url('src/static/menuImgs/dexBg/이브이.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/O4Jd_KajtxifyyLFO1C3ojCGtHl-pKscr3324lDb_eUZBe0c-_ED8YwDBqmYG9pT9jBe916D4lIZ637hHomEBw.webp",
@@ -21,8 +21,7 @@ export const menuAPI = [
         "linear-gradient(rgba(0, 84, 161, 0.7), rgba(0, 84, 161, 0.7)), url('src/static/menuImgs/샤미드.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(0, 84, 161, 0.7), rgba(0, 84, 161, 0.7)), url('src/static/menuImgs/dexBg/샤미드.webp')",
+      backgroundImage: "url('src/static/menuImgs/dexBg/샤미드.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/Z2ZtH0QG_GWWB-BKEw-lgk0Lewb-nv8jctwteTEVAtXYbPWbjlTrbLPnUB5no5J5Vp4nznnHPQ5jH_vgwn-S8A.webp",
@@ -35,8 +34,7 @@ export const menuAPI = [
         "linear-gradient(rgba(142, 144, 0, 0.7), rgba(142, 144, 0, 0.7)), url('src/static/menuImgs/쥬피썬더.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(142, 144, 0, 0.7), rgba(142, 144, 0, 0.7)), url('src/static/menuImgs/dexBg/쥬피썬더.webp')",
+      backgroundImage: "url('src/static/menuImgs/dexBg/쥬피썬더.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/_k5VLztKcq5w8cuuCIDs90FP3iUU1OWvE2Xi2fGGxC5d3DEdMAQ4_fzC9yqNvQOntEJEZM8gk_j9ZaclfgKf3Q.webp",
@@ -49,8 +47,7 @@ export const menuAPI = [
         "linear-gradient(rgba(166, 60, 0, 0.7), rgba(166, 60, 0, 0.7)), url('src/static/menuImgs/부스터.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(166, 60, 0, 0.7), rgba(166, 60, 0, 0.7)), url('src/static/menuImgs/dexBg/부스터.webp')",
+      backgroundImage:"url('src/static/menuImgs/dexBg/부스터.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/oF6E-p9wvx_0L4GkjUpLDPZimxTWfTK6omDzvRbFuvDdlzhP3v2CjdFPPftSW4zxlimuDcZrM_abVQ-idTuwvA.webp",
@@ -63,8 +60,7 @@ export const menuAPI = [
         "linear-gradient(rgba(173, 0, 168, 0.7), rgba(173, 0, 168, 0.7)), url('src/static/menuImgs/에브이.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(173, 0, 168, 0.7), rgba(173, 0, 168, 0.7)), url('src/static/menuImgs/dexBg/에브이.webp')",
+      backgroundImage: "url('src/static/menuImgs/dexBg/에브이.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/JtYBqIl3Ek5mtlDbkgE_HqbqIOFPeWZ0_kJoII3uo4aEpyA--DQWY482PAIL7WNpGLb2_kjRKoS4T7jZyoZ-oQ.webp",
@@ -77,8 +73,7 @@ export const menuAPI = [
         "linear-gradient(rgba(102, 102, 102, 0.7), rgba(102, 102, 102, 0.7)), url('src/static/menuImgs/블래키.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(102, 102, 102, 0.7), rgba(102, 102, 102, 0.7)), url('src/static/menuImgs/dexBg/블래키.webp')",
+      backgroundImage: "url('src/static/menuImgs/dexBg/블래키.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/oh4aXHAW47ZIKvqWUp1-qEJecFdCvdiwzsHXWM0S1cbQTs9CYs8Snon-HfqSxFgLDKx4ojIkHQBvHLYNt5pMAQ.webp",
@@ -91,8 +86,7 @@ export const menuAPI = [
         "linear-gradient(rgba(38, 144, 0, 0.7), rgba(38, 144, 0, 0.7)), url('src/static/menuImgs/리피아.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(38, 144, 0, 0.7), rgba(38, 144, 0, 0.7)), url('src/static/menuImgs/dexBg/리피아.webp')",
+      backgroundImage:"url('src/static/menuImgs/dexBg/리피아.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/4Bb7NkJX5TM9Knmgpa5JbvqGYiYDHfwlsrmflf6Xo7qG5u-hzaZ79wO-tuTdpUUatvnZyFDrHqnL8l6zBCI65w.webp",
@@ -105,8 +99,7 @@ export const menuAPI = [
         "linear-gradient(rgba(39, 172, 164, 0.7), rgba(39, 172, 164, 0.7)), url('src/static/menuImgs/글레이시아.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(39, 172, 164, 0.7), rgba(39, 172, 164, 0.7)), url('src/static/menuImgs/dexBg/글레이시아.webp')",
+      backgroundImage:"url('src/static/menuImgs/dexBg/글레이시아.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/6cSYp5KHATKoYhNKFwrJKgbH6jwkePz9kmJ1Cuwn8gxqxqib5YK56WjZjbA77NH8CXspT3t_mKoaFdVgCoeouQ.webp",
@@ -119,8 +112,7 @@ export const menuAPI = [
         "linear-gradient(rgba(169, 98, 128, 0.7), rgba(169, 98, 128, 0.7)), url('src/static/menuImgs/님피아.webp')",
     },
     dexBacground: {
-      backgroundImage:
-        "linear-gradient(rgba(169, 98, 128, 0.7), rgba(169, 98, 128, 0.7)), url('src/static/menuImgs/dexBg/님피아.webp')",
+      backgroundImage:"url('src/static/menuImgs/dexBg/님피아.webp')",
     },
     modelImg:
       "https://i.namu.wiki/i/nvZCS-dV6_XQnBtwIQCKQlWoAmeZRaIftT2WiZ5rX29qDJcFl9XRlYE0e24_j4LSkV9RK3IVmTMAfLmjVmxcfg.webp",
