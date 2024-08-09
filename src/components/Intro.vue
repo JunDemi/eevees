4a92c78f3be0beb50de08380c5ba12d68959293f
JunDemi <jungwook3176@gmail.com>
2024-08-09 13:11:08 +0900

    feat: axios패치 메소드 부모컴포넌트에서 작동 후 반환 값 props로 전달. 오디오에 key값 부여하여 울음소리 구분

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,76 +1,35 @@
 <template>
     <div class="intro">
-        <img :src="'/src/static/3D_Models/' + sendPokemon + '.gif'" />
+        <img :src="'/src/static/3D_Models/' + pokemonName + '.gif'" />
         <div class="pokemon-name">
-            <h2>{{ sendPokemon}}</h2>
-            <p>진화포켓몬</p>
+            <h2>{{ pokemonName }}</h2>
+            <p>{{ pokemonProps.genus }}</p>
+            <audio :key="pokemonProps.voice" ref="audioPlayer" :src="pokemonProps.voice" type="audio/ogg"></audio>
+            <button @click="playAudio">울음소리</button>
+
         </div>
+
     </div>
 </template>
 <script>
-// useEffect(() => {
-//   const fetchData = async () => {
-//     const allPokemonData = [];
-//     for (let i = 1; i <= 151; i++) {
-//       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
-//       const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`);
-//       const koreanName = speciesResponse.data.names.find(name => name.language.name === 'ko');
-//       allPokemonData.push({ ...response.data, korean_name: koreanName.name });
-//     }
-//     setPokemonData(allPokemonData);
-//   };
-
-//   fetchData();
-// }, []);
 export default {
     props: {
-        sendPokemon: String,
-    },
-    //데이터가 변화할때를 감지
-    watch: {
-        sendPokemon: { //부모 컴포넌트에서 전달받은 props가 변화할 때
-            immediate: true, // 컴포넌트가 처음 렌더링될 때도 실행
-            handler(sendName) { //핸들러의 매개변수 = props
-                this.getPokemonData(sendName);
-            }
-        }
+        pokemonProps: Object,
+        pokemonName: String,
     },
     data() {
         return {
-            pokemonName: {}
+
         }
     },
     methods: {
-        //이름 포맷 (이브이 -> eevee)
-        nameFormat(sendName) {
-            switch (sendName) {
-                case "이브이":
-                    return "eevee"
-                case "샤미드":
-                    return "vaporeon"
-                case "쥬피썬더":
-                    return "jolteon"
-                case "부스터":
-                    return "flareon"
-                case "에브이":
-                    return "espeon"
-                case "블래키":
-                    return "umbreon"
-                case "리피아":
-                    return "leafeon"
-                case "글레이시아":
-                    return "glaceon"
-                case "님피아":
-                    return "sylveon"
+        playAudio() {
+            const audio = this.$refs.audioPlayer;
+            if (audio) {
+                audio.play();
             }
-        },
-        //fetch메소드
-        getPokemonData(sendName) {
-            fetch(`${import.meta.env.VITE_BASE_URL}/${this.nameFormat(sendName)}`)
-                .then(response => response.json())
-                .then(data => this.pokemonName = data)
-        },
-    }
+        }
+    },
 }
 </script>
 <style></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,10 @@
 <template>
     <section class="home-container">
-        <Intro :sendPokemon="clickMenu"/>
+        <Intro :pokemonProps="pokemonData" :pokemonName="clickMenu"/>
         <div class="view">{{ clickMenu }}</div>
         <div class="menu-list">
             <span v-for="pokemon in menuArray" :style="pokemon.menuStyle" :key="pokemon.name"
-                @click="changeMenu(pokemon.name)"
-                :class="{active: pokemon.name === clickMenu ? true : false}"
-                >
+                @click="changeMenu(pokemon.name)" :class="{ active: pokemon.name === clickMenu ? true : false }">
                 <img :src="pokemon.modelImg" alt="" width="70px" height="70px" />
                 <p>{{ clickMenu === pokemon.name ? pokemon.name : "" }}</p>
             </span>
@@ -17,6 +15,7 @@
     </video>
 </template>
 <script>
+import axios from 'axios';
 import { menuAPI } from '../api/menuAPI';
 import '../assets/home.scss';
 import Intro from '../components/Intro.vue';
@@ -26,24 +25,75 @@ export default {
     components: { //컴포넌트
         Intro
     },
+    watch: {
+        clickMenu: { //부모 컴포넌트에서 전달받은 props가 변화할 때
+            immediate: true, // 컴포넌트가 처음 렌더링될 때도 실행
+            handler(clickMenu) { //핸들러의 매개변수 = props
+                this.getPokemonData(clickMenu);
+            },
+        },
+    },
     data() {
         return {
             //현재 선택된 메뉴
             clickMenu: "이브이",
             //메뉴 배열
-            menuArray: menuAPI
+            menuArray: menuAPI,
+            pokemonData: {
+                genus: "", //포켓몬 별명
+                type: "", //포켓몬 타입
+                id: 0, //포켓몬 도감번호 
+                voice: "" //포켓몬 울음소리
+            }
         }
     },
     methods: {
         //메뉴 선택 메소드
         changeMenu(name) {
             this.clickMenu = name;
-        }
+        },
+        //이름 포맷 (이브이 -> eevee)
+        nameFormat(sendName) {
+            switch (sendName) {
+                case "이브이":
+                    return "eevee"
+                case "샤미드":
+                    return "vaporeon"
+                case "쥬피썬더":
+                    return "jolteon"
+                case "부스터":
+                    return "flareon"
+                case "에브이":
+                    return "espeon"
+                case "블래키":
+                    return "umbreon"
+                case "리피아":
+                    return "leafeon"
+                case "글레이시아":
+                    return "glaceon"
+                case "님피아":
+                    return "sylveon"
+            }
+        },
+        //fetch메소드
+        async getPokemonData(sendName) {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/pokemon/${this.nameFormat(sendName)}`); //포켓몬 기본 정보
+            const speciesResponse = await axios.get(response.data.species.url); //포켓몬의 종
+            const koreanGenus = await speciesResponse.data.genera.find(gen => gen.language.name === 'ko'); //한국어: 포켓몬 별명
+
+            this.pokemonData = {
+                genus: koreanGenus.genus,
+                type: response.data.types[0].type.name,
+                id: response.data.id,
+                voice: response.data.cries.latest
+            }
+        },
     }
 }
 </script>
 <style scoped>
-    span.active{
-        width: 250px;
-    }
+span.active {
+    width: 250px;
+    border-color: #fff;
+}
 </style>
