1c42509f77f4024a6c720ceabcf57b0861ce39b0
JunDemi <jungwook3176@gmail.com>
2024-08-25 02:24:07 +0900

    feat: vue3 캐러셀 라이브러리 설치 후 photoContainer 컴포넌트에 적용

--- src/components/PhotoContainer.vue
+++ src/components/PhotoContainer.vue
@@ -1,32 +1,73 @@
 <script>
+import { ref } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+import { menuAPI } from "../api/menuAPI";
+
 export default {
-    props: {
-        pokemonProps: Object,
-        pokemonNo: Number,
-        menuArray: Array,
+	components: {
+		Carousel,
+		Slide,
+	},
+    setup() {
+        const myCarousel = ref(null);
+        return {
+            myCarousel
+        }
     },
     data() {
         return {
-            photoData: [
-                "/static/carousel/이브이.webp",
-                "/static/carousel/샤미드.webp",
-                "/static/carousel/쥬피썬더.webp",
-                "/static/carousel/부스터.webp",
-                "/static/carousel/에브이.webp",
-                "/static/carousel/블래키.webp",
-                "/static/carousel/리피아.webp",
-                "/static/carousel/글레이시아.webp",
-                "/static/carousel/님피아.webp",
-            ],
+            carouselArray: menuAPI
+            // photoData: [
+            //     "../static/carousel/이브이.webp", 
+            //     "../static/carousel/샤미드.webp", 
+            //     "../static/carousel/쥬피썬더.webp", 
+            //     "../static/carousel/부스터.webp", 
+            //     "../static/carousel/에브이.webp", 
+            //     "../static/carousel/블래키.webp", 
+            //     "../static/carousel/리피아.webp", 
+            //     "../static/carousel/글레이시아.webp", 
+            //     "../static/carousel/님피아.webp"
+            // ],
         }
+    },
+     methods: {
+        handleStart(data) {
+            if(data.slidingToIndex >= data.slidesCount) {
+                console.log(0)
+            }
+            else if(data.slidingToIndex < 0) {
+                console.log(data.slidesCount - 1)
+            }
+            else{
+                console.log(data.slidingToIndex)
+            }
+        },
     }
 }
 </script>
 <template>
-    <div class="carousel">
-        <div v-for="(photo, index) in photoData" :key="photo">
-            <img :src="'src/' + photo" alt="존재하지 않는 이미지들" width="200px"/>
+    <div class="pokemon-carousel-container">
+        <div class="slide-container">
+            <Carousel :itemsToShow="3" :wrapAround="true" :transition="500" ref="myCarousel"
+            @slide-start="handleStart">
+			<Slide v-for="data in carouselArray" :key="data.no">
+				<div class="carousel__item">
+					<img class="slideImg" :src="'src/static/carousel/'+ data.name + '.webp'" width="300px" height="300px"/>
+				</div>
+			</Slide>
+		</Carousel>
+        </div>
+        <div class="slide-buttons">
+            <div>
+                <button v-for="index in carouselArray.slice(0, 4)" @click="myCarousel.slideTo(index.no)">클릭</button>
+            </div>
+            <div>
+                <button v-for="index in carouselArray.slice(4, 9)" @click="myCarousel.slideTo(index.no)">클릭</button>
+            </div>
         </div>
     </div>
 </template>
-<style scoped></style>
+
+<style>
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,78 @@
-<template lang="">
-    <div>
-        
-    </div>
-</template>
 <script>
+// import { ref } from "vue";
+// import { Carousel, Pagination, Navigation, Slide } from "vue3-carousel";
+// import home_1 from "../static/carousel/이브이.webp";
+// import home_2 from "../static/carousel/샤미드.webp";
+// import home_3 from "../static/carousel/쥬피썬더.webp";
+// import home_4 from "../static/carousel/부스터.webp";
+// import home_5 from "../static/carousel/에브이.webp";
+// import home_6 from "../static/carousel/블래키.webp";
+// import home_7 from "../static/carousel/리피아.webp";
+// import home_8 from "../static/carousel/글레이시아.webp";
+// import home_9 from "../static/carousel/님피아.webp";
+
+//import "vue3-carousel/dist/carousel.css";
+
 export default {
-    
-}
+	name: "About",
+	// components: {
+	// 	Carousel,
+	// 	Slide,
+    //     Navigation,
+	// 	Pagination,
+	// },
+    // setup() {
+    //     const myCarousel = ref(null);
+    //     return {
+    //         myCarousel
+    //     }
+    // },
+	// data() {
+	// 	return {
+	// 		slides: [home_1, home_2, home_3, home_4, home_5, home_6, home_7, home_8, home_9],
+	// 	};
+	// },
+    // methods: {
+    //     handleStart(data) {
+    //         if(data.slidingToIndex >= data.slidesCount) {
+    //             console.log(0)
+    //         }
+    //         else if(data.slidingToIndex < 0) {
+    //             console.log(data.slidesCount - 1)
+    //         }
+    //         else{
+    //             console.log(data.slidingToIndex)
+    //         }
+    //     },
+    //     // handleEnd(data) {
+    //     //     console.log(data)
+    //     // },
+    //     // handleLoop(data){
+    //     //     console.log(data)
+    //     // }
+    // }
+};
 </script>
-<style lang="">
-    
+<template>
+    <!-- <div class="container">
+        <div class="wrapper">
+		<Carousel :itemsToShow="3" :wrapAround="true" :transition="500" ref="myCarousel"
+            @slide-start="handleStart">
+			<Slide v-for="slide in slides" :key="slide">
+				<div class="carousel__item">
+					<img class="slideImg" :src="slide" width="100px"/>
+				</div>
+			</Slide>
+		</Carousel>
+
+        <button @click="myCarousel.slideTo(8)">클릭</button>
+        
+	</div>
+    </div> -->
+	
+</template>
+
+
+<style>
+
 </style>
